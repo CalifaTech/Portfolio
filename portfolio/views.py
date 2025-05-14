7ca3c9ba34f4d6f8abfb15d596b4da112312c601
CalifaTech <camrynewilson@gmail.com>
2025-05-14 17:00:30 -0500

    Portfolio Views

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. Welcome to my portfolio!")
+
+def resume(request):
+    return HttpResponse("This is my resume page.")
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
+
+def work(request):
+    return HttpResponse("This is the projects page.")
