4de6f125e96dee670d0776f59e91f99d599e6f36
CalifaTech <camrynewilson@gmail.com>
2025-05-14 14:17:41 -0500

    Portfolio Initial Model Migration

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Website(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
